377af06c42117858f4d07372f9b098b4c8c167dd
Matt Davis <mattdavis@Matts-MacBook-Air.local>
2018-05-04 17:56:42 +0100

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+	public float MoveSpeed;
+	public float RotateSpeed;
+
+	private bool isGrounded = false;
+	private Rigidbody rigidBody;
+
+	public Vector3 jump;
+	public float jumpForce = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		this.Spawn ();	
+		this.rigidBody = GetComponent<Rigidbody> ();
+		this.jump = new Vector3 (0.0f, this.jumpForce, 0);
+	}
+
+	void Spawn() {
+		this.gameObject.transform.position = new Vector3 (0, 1.2f, 0);
+	}
+		
+
+	void OnCollisionStay()
+	{
+		isGrounded = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
+
+			this.rigidBody.AddForce(jump * jumpForce, ForceMode.Impulse);
+			isGrounded = false;
+		}
+
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * RotateSpeed;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * MoveSpeed;
+
+		transform.Rotate(0, x, 0);
+		transform.Translate(0, 0, z);
+	}
+}
---
+++ Assets/Scripts/Generation/FloorGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorGenerator : MonoBehaviour {
+
+	public GameObject WhiteTilePrefab;
+	public GameObject BlackTilePrefab;
+
+	public int Width;
+	public int Height;
+
+	// Use this for initialization
+	void Start () {
+		this.Generate ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public void Generate() {
+		int counter = 0;
+		for (int x = 0; x < Width; x++) {
+			for (int y = 0; y < Height; y++) {
+				bool isWhite = counter % 2 == 0;
+				var tilePrefab = isWhite ? WhiteTilePrefab : BlackTilePrefab;
+				var position = new Vector3 (x, 0, y);
+				var tile = Instantiate (tilePrefab, position, Quaternion.identity);
+				counter++;
+			}
+		}
+	}
+}
