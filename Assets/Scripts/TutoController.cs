32ddbe781f7e8e9e16cbc00b25f3a1a5c47e9107
Jean-Luc Mourey <moureyjeanluc@gmail.com>
2018-05-14 19:45:35 +0100

    started control tutorial design

--- Assets/Scripts/TutoController.cs
+++ Assets/Scripts/TutoController.cs
@@ -4,13 +4,48 @@ using UnityEngine;
 
 public class TutoController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private GameObject RightTrigger, LeftTrigger, LeftStick;
+    private bool LT, RT, LStr, LStl, LSbr, LSbl;
+
+    private float x, z;
+
+    // Use this for initialization
+    void Start () {
+        LT = RT = LStr = LStl = LSbr = LSbl = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+
+        if (x != 0 && z != 0)
+        {
+            if (x > 0)
+            {
+                if (z > 0)
+                    ;
+                else if (z < 0)
+                    ;
+            }
+            else if (x < 0)
+            {
+                if (z > 0)
+                    ;
+                else if (z < 0)
+                    ;
+            }
+        }
+
+        if (Input.GetAxis("Right Trigger") == 1) {
+            RT = true;
+        }
+        if (Input.GetAxis("Left Trigger") == 1)
+        {
+            LT = true;
+        }
+    }
 }
