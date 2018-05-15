357f1c5b5dd9e31302f171b4a9de0565975af1e8
Jean-Luc Mourey <moureyjeanluc@gmail.com>
2018-05-15 02:20:19 +0100

    added menu prototype
    still needs to stop playerinput and blur the screen

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    public GameObject GUICanvas, MenuCanvas, camera, gameObjects;
+
+    private bool menuActive = false;
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            menuActive = !menuActive;
+        }
+
+        GUICanvas.SetActive(!menuActive);
+        //gameObjects.SetActive(!menuActive);
+        MenuCanvas.SetActive(menuActive);
+
+        if (menuActive)
+        {
+
+        }
+
+        /*if (menuActive)
+            camera.GetComponent<Camera>().cullingMask = 0;
+        else
+            camera.GetComponent<Camera>().cullingMask = 1;*/
+    }
+}
